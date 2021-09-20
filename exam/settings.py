e5f2f64ad57600affe8c714af740d2b173695142
mchadala <mchadala@asu.edu>
2021-09-20 03:40:13 -0700

    added completed project code

--- exam/settings.py
+++ exam/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'exam.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'online_exam',
-        'USER':'root',
-        'PASSWORD':'sivani',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
